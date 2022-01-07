4e9255ec3ecc4f60408d89ccd80574805e4d564e
Ngo Quoc Dat <datlechin@gmail.com>
2022-01-07 16:08:14 +0700

    Extract `index.js` to child components

---
+++ js/src/forum/components/addChangeBirthdaySettingsPage.js
@@ -0,0 +1,16 @@
+import app from 'flarum/forum/app';
+import { extend } from 'flarum/common/extend';
+import SettingsPage from 'flarum/forum/components/SettingsPage';
+import Button from 'flarum/common/components/Button';
+import ChangeBirthdayModal from '../components/ChangeBirthdayModal';
+
+export default function addChangeBirthdaySettingsPage() {
+  extend(SettingsPage.prototype, 'accountItems', function (items) {
+    items.add(
+      'changeBirthday',
+      <Button className="Button" onclick={() => app.modal.show(ChangeBirthdayModal)}>
+        {app.translator.trans('datlechin-birthdays.forum.change_dob.title')}
+      </Button>
+    );
+  });
+}
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,65 +1,16 @@
 import app from 'flarum/forum/app';
-import { extend } from 'flarum/common/extend';
 import User from 'flarum/common/models/User';
 import Model from 'flarum/common/Model';
-import SettingsPage from 'flarum/forum/components/SettingsPage';
-import UserCard from 'flarum/forum/components/UserCard';
-import SessionDropdown from 'flarum/forum/components/SessionDropdown';
-import Button from 'flarum/common/components/Button';
-import icon from 'flarum/common/helpers/icon';
-import ChangeBirthdayModal from './components/ChangeBirthdayModal';
+import addBirthdayUserCard from './components/addBirthdayUserCard';
+import addChangeBirthdaySettingsPage from './components/addChangeBirthdaySettingsPage';
+import addBirthdaySessionDrodpown from './components/addBirthdaySessionDropdown';
 
 app.initializers.add('datlechin/flarum-birthdays', () => {
   User.prototype.birthday = Model.attribute('birthday');
   User.prototype.showDobDate = Model.attribute('showDobDate');
   User.prototype.showDobYear = Model.attribute('showDobYear');
-  extend(SettingsPage.prototype, 'accountItems', function (items) {
-    items.add(
-      'changeBirthday',
-      <Button className="Button" onclick={() => app.modal.show(ChangeBirthdayModal)}>
-        {app.translator.trans('datlechin-birthdays.forum.change_dob.title')}
-      </Button>
-    );
-  });
-  extend(UserCard.prototype, 'infoItems', function (items) {
-    const user = this.attrs.user;
-    const userLocale = user.preferences()?.locale || app.translator.formatter.locale;
-    let birthday = user.birthday();
-    let age;
-    const date = new Date(birthday);
-    const options = {
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric',
-    };
 
-    if (birthday === '0000-00-00') return;
-
-    if (user.showDobDate() && user.showDobYear()) {
-      age = new Date().getFullYear() - new Date(birthday).getFullYear();
-      birthday = date.toLocaleDateString(userLocale, options);
-    } else if (user.showDobDate() === true && user.showDobYear() === false) {
-      birthday = date.toLocaleDateString(userLocale, options);
-      birthday = birthday.split(',')[0];
-    } else {
-      return;
-    }
-
-    items.add(
-      'birthday',
-      <>
-        {icon('fas fa-birthday-cake')}
-        <span className="birthday">{app.translator.trans('datlechin-birthdays.forum.user.birthday_text', { date: birthday })}</span>
-        {user.showDobYear() ? <span className="age">({app.translator.trans('datlechin-birthdays.forum.user.age_text', { age: age })})</span> : null}
-      </>
-    );
-  });
-  extend(SessionDropdown.prototype, 'items', function (items) {
-    items.add(
-      'birthday',
-      <Button icon="fas fa-birthday-cake" onclick={() => app.modal.show(ChangeBirthdayModal)}>
-        {app.translator.trans('datlechin-birthdays.forum.header.edit_dob_button')}
-      </Button>
-    );
-  });
+  addBirthdayUserCard();
+  addChangeBirthdaySettingsPage();
+  addBirthdaySessionDrodpown();
 });
